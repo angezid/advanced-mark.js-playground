b175d2e82c3457adc1581740fde8ceedf4537fff
angezid <82150610+angezid@users.noreply.github.com>
2026-04-05 03:43:49 +0800

    improve scripts

--- scripts/copy-to-playground.js
+++ scripts/copy-to-playground.js
@@ -13,6 +13,14 @@ copyFile();
 function copyFile() {
     let success = true;
     try {
+        ['css', 'html', 'js'].forEach((name) => {
+            let path = 'playground/' + name;
+
+            if ( !fs.existsSync(path)) {
+                fs.mkdirSync(path, { recursive: true });
+            }
+        });
+
         for (let i = 0; i < files.length; i++) {
             fs.copyFileSync(sourceHtmlDir + files[i], destHtmlDir + files[i], fs.constants.COPYFILE_FICLONE);
         }
@@ -20,7 +28,6 @@ function copyFile() {
         fs.copyFileSync('src/css/main.css', 'playground/css/main.css', fs.constants.COPYFILE_FICLONE);
 
         let html = fs.readFileSync('src/index.html', 'utf-8');
-
         // updates playground version; corrects scripts pathes
         html = html.replace(/(<[a-z]+>advanced-mark\.js playground\s+v)[\d.]+/, '$1' + version);
         html = html.replace(/<script [^]+<\/script>/, '<script src="js/plugins.js"></script>\n<script src="js/main.js"></script>');
--- scripts/minify-plugins.js
+++ scripts/minify-plugins.js
@@ -14,30 +14,45 @@ const debugPlayground = false;
 process();
 
 async function process() {
-	let scripts = '';
-
-	for (let i = 0; i < files3.length; i++) {
-		const code = fs.readFileSync(source3Dir + files3[i], 'utf-8');
-		const result = await minify(code.trim());
-
-		scripts += result.code + '\n\n';
-	}
-	fs.writeFileSync(destDir + 'plugins.js', scripts);
-
-	scripts = '';
-
-	for (let i = 0; i < files.length; i++) {
-		const code = fs.readFileSync(sourceDir + files[i], 'utf-8');
-
-		scripts += '/*!****************************\n* ' + files[i] + '\n******************************/\n';
-
-		if (debugPlayground) {
-			scripts += code + '\n\n';
-
-		} else {
+	let success = true;
+	try {
+		if ( !fs.existsSync(destDir)) {
+			fs.mkdirSync(destDir, { recursive: true });
+		}
+		
+		let scripts = '';
+	
+		for (let i = 0; i < files3.length; i++) {
+			const code = fs.readFileSync(source3Dir + files3[i], 'utf-8');
 			const result = await minify(code.trim());
+	
 			scripts += result.code + '\n\n';
 		}
+		fs.writeFileSync(destDir + 'plugins.js', scripts);
+	
+		scripts = '';
+	
+		for (let i = 0; i < files.length; i++) {
+			const code = fs.readFileSync(sourceDir + files[i], 'utf-8');
+	
+			scripts += '/*!****************************\n* ' + files[i] + '\n******************************/\n';
+	
+			if (debugPlayground) {
+				scripts += code + '\n\n';
+	
+			} else {
+				const result = await minify(code.trim());
+				scripts += result.code + '\n\n';
+			}
+		}
+		fs.writeFileSync(destDir + 'main.js', scripts);
+		
+	} catch(e) {
+		console.log(e);
+		success = false;
+	} 
+	
+	if (success) {
+		console.log('The playground was minified successfully.');
 	}
-	fs.writeFileSync(destDir + 'main.js', scripts);
 }
